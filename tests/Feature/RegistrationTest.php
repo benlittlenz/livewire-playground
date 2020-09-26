998e8bbe81c3a2a27bc12ac77468d81b3c1c637e
benlittlenz <ben@pop-os.localdomain>
2020-09-26 19:14:40 +1200

    added tests for password registration

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use Livewire\Livewire;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegistrationTest extends TestCase
@@ -36,4 +37,64 @@ class RegistrationTest extends TestCase
             ->call('register')
             ->assertHasErrors(['email' => 'required']);
     }
+
+    /** @test */
+    function email_is_valid_email()
+    {
+        Livewire::test('auth.register')
+            ->set('email', 'pumpkins')
+            ->set('password', 'secret')
+            ->set('passwordConfirmation', 'secret')
+            ->call('register')
+            ->assertHasErrors(['email' => 'email']);
+    }
+
+    /** @test */
+    function email_is_unique()
+    {
+        User::create([
+            'email' => 'frank@mail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        Livewire::test('auth.register')
+            ->set('email', 'frank@mail.com')
+            ->set('password', 'secret')
+            ->set('passwordConfirmation', 'secret')
+            ->call('register')
+            ->assertHasErrors(['email' => 'unique']);
+    }
+
+    /** @test */
+    function password_is_required()
+    {
+        Livewire::test('auth.register')
+            ->set('email', 'frank@mail.com')
+            ->set('password', '')
+            ->set('passwordConfirmation', 'secret')
+            ->call('register')
+            ->assertHasErrors(['password' => 'required']);
+    }
+
+        /** @test */
+        function password_is_min_six_chars()
+        {
+            Livewire::test('auth.register')
+                ->set('email', 'frank@mail.com')
+                ->set('password', 'secre')
+                ->set('passwordConfirmation', 'secret')
+                ->call('register')
+                ->assertHasErrors(['password' => 'min']);
+        }
+
+                /** @test */
+                function passwords_matches()
+                {
+                    Livewire::test('auth.register')
+                        ->set('email', 'frank@mail.com')
+                        ->set('password', 'secret')
+                        ->set('passwordConfirmation', 'not-secret')
+                        ->call('register')
+                        ->assertHasErrors(['password' => 'same']);
+                }
 }
