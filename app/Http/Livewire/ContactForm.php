2ea7a700376ac493dda18edb13d3b5bf2ec57a46
benlittlenz <ben@pop-os.localdomain>
2020-10-04 12:41:41 +1300

    added success message on email sent

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -13,6 +13,7 @@ class ContactForm extends Component
     public $email;
     public $phone;
     public $message;
+    public $successMessage;
 
     public function submitForm()
     {
@@ -30,9 +31,11 @@ class ContactForm extends Component
 
         Mail::to('andre@andre.com')->send(new ContactFormMailable($contact));
 
+        $this->successMessage = 'We received your message successfully and will get back to you shortly!';
+
         $this->resetForm();
 
-        // return back()->with('success_message', 'We received your message successfully and will get back to you shortly!');
+        //session()->flash('success_message', );
     }
 
     private function resetForm()
--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -76,8 +76,8 @@
                             <div class="ml-auto pl-3">
                                 <div class="-mx-1.5 -my-1.5">
                                     <button
+                                        wire:click="$set('successMessage' , null)"
                                         type="button"
-                                        wire:click="$set('successMessage', null)"
                                         class="inline-flex rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:bg-green-100 transition ease-in-out duration-150"
                                         aria-label="Dismiss">
                                         <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
