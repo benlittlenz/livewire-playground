acda59ac8cfb9e917a5ae48a6df9868c8a34ebf3
benlittlenz <ben@pop-os.localdomain>
2020-10-04 16:51:15 +1300

    added test cases for hitting itunes API

---
+++ tests/Feature/SearchDropdownTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Livewire\SearchDropdown;
+use Tests\TestCase;
+use Livewire\Livewire;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SearchDropdownTest extends TestCase
+{
+    /** @test */
+    public function search_dropdown_searches_correctly_if_song_exists()
+    {
+        Livewire::test(SearchDropdown::class)
+            ->assertDontSee('John Lennon')
+            ->set('search', 'Imagine')
+            ->assertSee('John Lennon');
+    }
+
+    /** @test */
+    public function search_dropdown_shows_message_if_no_song_exists()
+    {
+        Livewire::test(SearchDropdown::class)
+        ->set('search', 'ttyhhghfg5646hfghfghfhhghgf')
+        ->assertSee('No results found for');
+    }
+}
