652700c72b844225149301c08a4be9bad7357fa4
benlittlenz <ben@pop-os.localdomain>
2020-10-06 15:37:23 +1300

    added method to create and validate comment

--- app/Http/Livewire/CommentSection.php
+++ app/Http/Livewire/CommentSection.php
@@ -3,17 +3,37 @@
 namespace App\Http\Livewire;
 
 use App\Models\Post;
+use App\Models\Comment;
 use Livewire\Component;
 
 class CommentSection extends Component
 {
     public $post;
+    public $comment;
     public $successMessage;
 
-    // public function mount(Post $post)
-    // {
-    //     $this->post = $post;
-    // }
+    protected $rules = [
+        'comment' => 'required|min:4',
+        'post' => 'required',
+    ];
+
+    public function createComment()
+    {
+        $this->validate();
+
+        sleep(1);
+        Comment::create([
+            'post_id' => $this->post->id,
+            'username' => 'Guest',
+            'content' => $this->comment,
+        ]);
+
+        $this->comment = '';
+
+        $this->post = Post::find($this->post->id);
+
+        $this->successMessage =  'Comment was posted!';
+    }
 
     public function render()
     {
